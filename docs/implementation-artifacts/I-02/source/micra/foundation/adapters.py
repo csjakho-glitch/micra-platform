16d093a6356638cd90d5644c7bfc20bfb5cf2d78
csjakho-glitch <cs.jakho@gmail.com>
2026-08-21 16:19:28 +0700

    docs(i02): preserve adapter contract references

---
+++ docs/implementation-artifacts/I-02/source/micra/foundation/adapters.py
@@ -0,0 +1,11 @@
+class AdapterContractError(Exception):
+    pass
+
+class DFTAdapter:
+    name = "R0-11-DFT"
+
+class RiskAdapter:
+    name = "R0-12-Risk"
+
+class EngineeringAdapter:
+    name = "R0-13-Engineering"
